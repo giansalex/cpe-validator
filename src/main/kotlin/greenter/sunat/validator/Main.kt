a41fe8e7752524d7c9c9edc89d238f7c0a0fce9d
Giancarlos Salas <giansalex@gmail.com>
2019-06-16 23:02:18 -0500

    output result as json

--- src/main/kotlin/greenter/sunat/validator/Main.kt
+++ src/main/kotlin/greenter/sunat/validator/Main.kt
@@ -1,8 +1,14 @@
 package greenter.sunat.validator
 
+import greenter.sunat.validator.parser.ErrorResultParser
 import greenter.sunat.validator.process.SaxonXmlValidator
+import com.google.gson.Gson
 
 fun main(args: Array<String>) {
-    val validator = SaxonXmlValidator()
-    validator.validate(args[0], args[1])
+    val validator = SaxonXmlValidator(ErrorResultParser())
+    val result = validator.validate(args[0], args[1])
+
+    val gson = Gson()
+
+    print(gson.toJson(result))
 }
--- src/main/kotlin/greenter/sunat/validator/model/ErrorResult.kt
+++ src/main/kotlin/greenter/sunat/validator/model/ErrorResult.kt
@@ -1,9 +1,13 @@
 package greenter.sunat.validator.model
 
+import com.google.gson.annotations.SerializedName
+
 class ErrorResult {
     var code: String? = null
     var message: String? = null
     var level: ErrorLevel? = null
+    @SerializedName("node_path")
     var nodePath: String? = null
+    @SerializedName("node_value")
     var nodeValue: String? = null
 }
