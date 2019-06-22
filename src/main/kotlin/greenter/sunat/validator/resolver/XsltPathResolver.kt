88a8f576fdbc51d2d95c7e9670e1b485126a7811
Giancarlos Salas <giansalex@gmail.com>
2019-06-22 17:09:13 -0500

    fix uppercase directory

--- src/main/kotlin/greenter/sunat/validator/resolver/XsltPathResolver.kt
+++ src/main/kotlin/greenter/sunat/validator/resolver/XsltPathResolver.kt
@@ -7,16 +7,16 @@ class XsltPathResolver(private val baseDirectory: String) : PathResolver {
     override fun getPath(type: DocumentType): String {
 
         val xslFile = when (type) {
-            DocumentType.FACTURA -> "2.x/ValidaExprRegFactura-2.0.1.xsl"
-            DocumentType.BOLETA -> "2.x/ValidaExprRegBoleta-2.0.1.xsl"
-            DocumentType.NOTA_CREDITO -> "2.x/ValidaExprRegNC-2.0.1.xsl"
-            DocumentType.NOTA_DEBITO -> "2.x/ValidaExprRegND-2.0.1.xsl"
-            DocumentType.RESUMEN_DIARIO -> "1.x/ValidaExprRegSummary-1.1.0.xsl"
-            DocumentType.COMUNICACION_BAJA -> "1.x/ValidaExprRegSummaryVoided-1.0.1.xsl"
-            DocumentType.GUIA_REMISION -> "1.x/ValidaExprRegGuiaRemitente-2.0.1.xsl"
-            DocumentType.RETENCION -> "1.x/ValidaExprRegRetencion-1.0.2.xsl"
-            DocumentType.PERCEPCION -> "1.x/ValidaExprRegPercepcion-1.0.1.xsl"
-            DocumentType.REVERSION -> "1.x/ValidaExprRegOtrosVoided-1.0.1.xsl"
+            DocumentType.FACTURA -> "2.X/ValidaExprRegFactura-2.0.1.xsl"
+            DocumentType.BOLETA -> "2.X/ValidaExprRegBoleta-2.0.1.xsl"
+            DocumentType.NOTA_CREDITO -> "2.X/ValidaExprRegNC-2.0.1.xsl"
+            DocumentType.NOTA_DEBITO -> "2.X/ValidaExprRegND-2.0.1.xsl"
+            DocumentType.RESUMEN_DIARIO -> "1.X/ValidaExprRegSummary-1.1.0.xsl"
+            DocumentType.COMUNICACION_BAJA -> "1.X/ValidaExprRegSummaryVoided-1.0.1.xsl"
+            DocumentType.GUIA_REMISION -> "1.X/ValidaExprRegGuiaRemitente-2.0.1.xsl"
+            DocumentType.RETENCION -> "1.X/ValidaExprRegRetencion-1.0.2.xsl"
+            DocumentType.PERCEPCION -> "1.X/ValidaExprRegPercepcion-1.0.1.xsl"
+            DocumentType.REVERSION -> "1.X/ValidaExprRegOtrosVoided-1.0.1.xsl"
         }
 
         return Paths.get(baseDirectory, xslFile).toString()
