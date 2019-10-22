6f807d749690e981056b950297c660a023637fe6
Giancarlos Salas <giansalex@gmail.com>
2019-10-22 10:43:57 -0500

    update xsl for SFS_v1.3

--- src/main/kotlin/greenter/sunat/validator/resolver/XsltPathResolver.kt
+++ src/main/kotlin/greenter/sunat/validator/resolver/XsltPathResolver.kt
@@ -14,7 +14,7 @@ class XsltPathResolver(private val baseDirectory: String) : PathResolver {
             DocumentType.RESUMEN_DIARIO -> "1.X/ValidaExprRegSummary-1.1.0.xsl"
             DocumentType.COMUNICACION_BAJA -> "1.X/ValidaExprRegSummaryVoided-1.0.1.xsl"
             DocumentType.GUIA_REMISION -> "1.X/ValidaExprRegGuiaRemitente-2.0.1.xsl"
-            DocumentType.RETENCION -> "1.X/ValidaExprRegRetencion-1.0.2.xsl"
+            DocumentType.RETENCION -> "1.X/ValidaExprRegRetencion-1.0.3.xsl"
             DocumentType.PERCEPCION -> "1.X/ValidaExprRegPercepcion-1.0.1.xsl"
             DocumentType.REVERSION -> "1.X/ValidaExprRegOtrosVoided-1.0.1.xsl"
         }
